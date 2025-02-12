84680dd01d52673d344009749d127aef6a59ae87
Agnibha Nanda <agnibhananda@gmail.com>
2025-02-12 19:13:58 +0530

    form

--- app/page.tsx
+++ app/page.tsx
@@ -965,7 +965,7 @@ export default function Home() {
 
           <motion.form 
             onSubmit={handleSubmit}
-            action="https://formsubmit.co/agnibhananda@gmail.com" 
+            action="61d24f09b8a68dc074217c64b1b1ea60" 
             method="POST"
             className="space-y-8"
             initial={{ opacity: 0, y: 20 }}
